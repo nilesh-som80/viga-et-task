e21d3acadee7872ecffc953ab34c63129d6066e6
Nilesh Singh <69386843+nilesh-som80@users.noreply.github.com>
2022-01-20 00:55:07 +0530

    Welcome PAge Editted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,10 @@
     <title>Task</title>
 </head>
 <body>
+    <div style="max-width:700px;justify-content:center;text-align:center;position:absolute">
+        <p>For more details click on the button below and follow the docs</p>
+    <a href="https://github.com/nilesh-som80/viga-et-task.git" style="border:1px solid rgb(184, 81, 33);border-radius:20%;text-decoration:none;color:rgb(196, 193, 15);padding:10px;font-style:San Francisco;">Repo Link</a>
+    </div>
+
 </body>
 </html>
